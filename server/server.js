0ba0280891b7e28000b3616e666101953f2bb142
Scott Bromander <scottbromander@Scotts-MacBook-Pro.local>
2019-05-01 09:29:45 -0500

    adds basic client files and serving from server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+// Uses express to create a server app
 const app = express();
+// Port used for connections, written as localhost:5000/ on a client app
 const PORT = 5000;
 
+// Serves static files like INDEX.html, CLIENT.js, and so on
+app.use(express.static('server/public'));
+
+// Turns on my server and listens for connections
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
 });
