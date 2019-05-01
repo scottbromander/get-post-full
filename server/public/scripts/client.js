83a97f521d0e2056090643bdf8f9b93a93fc0260
Scott Bromander <scottbromander@Scotts-MacBook-Pro.local>
2019-05-01 09:39:52 -0500

    Moves data off front end into client

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,31 @@
 $(document).ready(onReady);
 
 function onReady() {
-    console.log('Totes ready!');
+    $('.js-btn-addRestaurant').on('click', addRestaurant);
+}
+
+function addRestaurant() {
+    const name = $('#name').val();
+    const address = $('#address').val();
+    const bestfood = $('#bestfood').val();
+    
+    const restaurantObject = {
+        name,
+        address,
+        bestfood
+    }
+
+    console.log(restaurantObject);
+}
+
+function postRestaurant() {
+
+}
+
+function getRestaurants() {
+
+}
+
+function render() {
+
 }
