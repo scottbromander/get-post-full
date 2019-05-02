5d57bd4e6056916529807fe19f01502e2a5634ab
Scott Bromander <scottbromander@gmail.com>
2019-05-02 09:46:06 -0500

    removes notes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,38 +1,24 @@
 $(document).ready(onReady);
 
-// When document is ready, run this function
 function onReady() {
-    // Go get the restaurants inside the database
     getRestaurants();
     $('.js-btn-addRestaurant').on('click', addRestaurant);
 }
 
-// Takes the information from the Front End and moves it to
-// the client
 function addRestaurant() {
     const name = $('#name').val();
     const address = $('#address').val();
     const bestfood = $('#bestfood').val();
 
-    // Create an object, and store values,
-    // related to the input fields above.
-    // Note that the 'key name' is the same
-    // as the above variable names. This both
-    // creates a key and maps the value to the
-    // closest variable of the same name.
     const restaurantObject = {
         name,
         address,
         bestfood
     }
 
-    // Call postRestaurant giving it the object we created
-    // above
     postRestaurant(restaurantObject);
 }
 
-// Send information to server, to eventually be saved
-// to the database
 function postRestaurant(restaurantObject) {
     $.ajax({
         type: 'POST',
@@ -43,30 +29,19 @@ function postRestaurant(restaurantObject) {
     })
 }
 
-// Go to the database for the restaurant list
 function getRestaurants() {
-    // First part hops over to the server, with a 
-    // GET request to the url restaurants.
     $.ajax({
         type: 'GET',
         url: '/restaurants'
     }).then(function(arrayFromDatabase) {
-        // Take the data from the server (that we got from the
-        // database), and send that over to the render function.
         render(arrayFromDatabase);
     });
 }
 
 function render(arrayFromDatabase) {
-    // Find container, and wipe out all its children
     $('.container').empty();
 
-    // The argument above, called arrayFromDatabase,
-    // has an array of objects, and for each object
-    // do the things in the loop
     for (let restaurant of arrayFromDatabase) {
-        // Add a new div and children to container,
-        // for each item inside the array
         $('.container').append(`
             <div>
                 <h2>${restaurant.name}</h2>
--- server/server.js
+++ server/server.js
@@ -1,28 +1,19 @@
 const express = require('express');
-// Uses express to create a server app
 const app = express();
 
-// Brings bodyParser into our application
 const bodyParser = require('body-parser');
 
-// Port used for connections, written as localhost:5000/ on a client app
 const PORT = 5000;
 
-// Imports the router we created custom for '/restaurants' routes
 const restaurantRouter = require('./routes/restaurants.router');
 
-// Upgrading our server to get able to get JSON and other data,
-// and store it in req.body throughout the app.
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-// Serves static files like INDEX.html, CLIENT.js, and so on
 app.use(express.static('server/public'));
 
-// Anytime the client makes a '/restaurant' request use this router.
 app.use('/restaurants', restaurantRouter);
 
-// Turns on my server and listens for connections
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
 });
